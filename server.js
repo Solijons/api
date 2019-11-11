48b553d66e4a24a50f679328edd6055e4d8e9f8c
Solijons <solijons1996@gmail.com>
2019-11-10 14:13:47 -1000

    new changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.NODE_ENV || 3000;
+const PORT = process.env.NODE_ENV || 3000;
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(PORT);
